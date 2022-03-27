7d89685e7a388deecb7bfdd11f6ca98360d5e62c
iwona <iwo.jarek@gmail.com>
2022-03-27 13:48:31 +0200

    init project

---
+++ src/js/utils.js
@@ -0,0 +1,17 @@
+// /* global Handlebars, dataSource */
+
+// export const utils = {}; // eslint-disable-line no-unused-vars
+
+// utils.createDOMFromHTML = function(htmlString) {
+//   let div = document.createElement('div');
+//   div.innerHTML = htmlString.trim();
+//   return div.firstChild;
+// };
+
+// Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
+//   return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+// });
+  
+// Handlebars.registerHelper('joinValues', function(input, options) {
+//   return Object.values(input).join(options.fn(this));
+// });
