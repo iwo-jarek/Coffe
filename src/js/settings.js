cce9943be8dd7d7ef75b23c8e4494c1449366749
iwona <iwo.jarek@gmail.com>
2022-04-04 18:33:43 +0200

    pages

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,5 +1,5 @@
 export const select = {
-  
+
   containerOf: {
     pages: '#pages',
     products: 'products-wrapper',
@@ -26,11 +26,11 @@ export const classNames = {
 };
 
 export const settings = {
-  
+
   db: {
-    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
+    url: '//' + window.location.hostname + (window.location.hostname == 'localhost' ? ':3131' : ''),
     products: 'products',
-    
+
   },
 
 };
